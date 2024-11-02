e5a14ceee6cc07932567bd2bf4eabf99350620e0
httpsmojojojo <m.moiz17110@gmail.com>
2024-11-02 11:45:02 +0500

    Final Commit

---
+++ src/com/mojo/tilegame/sprites/Grub.java
@@ -0,0 +1,21 @@
+package com.mojo.tilegame.sprites;
+
+import com.mojo.graphics.Animation;
+
+/**
+    A Grub is a Creature that moves slowly on the ground.
+*/
+public class Grub extends Creature {
+
+    public Grub(Animation left, Animation right,
+        Animation deadLeft, Animation deadRight)
+    {
+        super(left, right, deadLeft, deadRight);
+    }
+
+
+    public float getMaxSpeed() {
+        return 0.05f;
+    }
+
+}
